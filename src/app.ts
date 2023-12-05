adb3b1d587221c26b88e20de6737d9ea021c5701
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-12-05 17:34:54 -0300

    chore: init project and install libs

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import "reflect-metadata";
+import express, { Application } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Application = express();
+
+export default app;
