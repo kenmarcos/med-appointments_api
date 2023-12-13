4c8ef56e6694212c6653e88bbf30a4e7ae707356
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2023-12-13 11:14:43 -0300

    doc: add initial api documentation with swagger

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,20 @@ import express, { Application } from "express";
 import dotenv from "dotenv";
 import { handleError } from "./middlewares";
 import router from "./routes";
+import swaggerUiExpress from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 
 dotenv.config();
 
 const app: Application = express();
 
 app.use(express.json());
+
+app.use(
+  "/api-documentation",
+  swaggerUiExpress.serve,
+  swaggerUiExpress.setup(swaggerDocument)
+);
 
 app.use("/api", router);
 
